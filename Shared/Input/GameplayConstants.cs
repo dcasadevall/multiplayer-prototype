149cc11f3d7af14c4bb06e039557a1bea620b535
Daniel Casadevall Pino <dcasadevall@me.com>
2025-08-06 20:11:56 +0200

    change projectile speed

--- Shared/Input/GameplayConstants.cs
+++ Shared/Input/GameplayConstants.cs
@@ -7,7 +7,7 @@ namespace Shared.Input
 
         // Laser/Projectile constants
         public const uint PlayerShotCooldownTicks = 15; // 0.5 seconds at 30 ticks/sec
-        public const float ProjectileSpeed = 1f;
+        public const float ProjectileSpeed = 8f;
         public const uint ProjectileTtlTicks = 120; // 4 seconds at 30 ticks/sec
         public const int ProjectileDamage = 25;
         public const uint MaxShotTickDeviation = 10; // Allow up to 10 ticks of deviation for shot validation
