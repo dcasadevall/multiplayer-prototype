ea544fc01aa1a35889b9817026d25b16b334f15b
Daniel Casadevall Pino <dcasadevall@me.com>
2025-08-10 00:58:06 +0200

    Targetcomponent is server only

--- Server/AI/TargetComponent.cs
+++ Server/AI/TargetComponent.cs
@@ -3,9 +3,12 @@ using Shared.ECS;
 
 namespace Server.AI
 {
-    public class TargetComponent : IComponent
+    /// <summary>
+    /// TargetComponent is used to identify the target entity for AI agents.
+    /// It contains the ID of the target entity that the AI should interact with or focus on.
+    /// </summary>
+    public class TargetComponent : IServerComponent
     {
         public Guid TargetId { get; set; }
     }
 }
-
