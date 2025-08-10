937a434969a73c7911c22ebf67e6deedcd63f88e
Daniel Casadevall Pino <dcasadevall@me.com>
2025-08-10 21:30:47 +0200

    import types

--- Shared/ECS/Archetypes/ProjectileArchetype.cs
+++ Shared/ECS/Archetypes/ProjectileArchetype.cs
@@ -2,6 +2,7 @@ using System.Numerics;
 using Shared.Damage;
 using Shared.ECS.Components;
 using Shared.ECS.Entities;
+using Shared.ECS.Simulation;
 using Shared.Physics;
 using Shared.Prediction;
 
